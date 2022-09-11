202d6c5f9f368d5530595b48241983a9bd8cbc00
Chidinma Akunwa <judithakunwa7@gmail.com>
2022-09-11 11:45:56 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ *  * main - the function main prints all alphabet
+ *     * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+
+{
+
+		int n;
+
+
+
+			for (n = 48; n <= 57; ++n)
+
+					{
+
+								putchar(n);
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
+}
